d546ed5e691e53d3bd3189fb7cd3f3fcabda8577
shuike <shuike007@126.com>
2023-09-20 14:14:09 +0800

    feat: cornerRadius、shadowColor

--- app/src/main/java/com/skit/cis/InnerShadowView.kt
+++ app/src/main/java/com/skit/cis/InnerShadowView.kt
@@ -3,34 +3,74 @@ package com.skit.cis
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Outline
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
 import android.widget.FrameLayout
 
 class InnerShadowView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-    companion object {
-        const val SHADOW_RADIUS = 20f
-        const val STROKE_WIDTH = 20f
-    }
-
+    private var shadowLineRect: RectF? = null
+    private var shadowColor: Int = Color.BLACK
     private val shadowPaint: Paint by lazy {
         Paint().apply {
             style = Paint.Style.STROKE
             strokeWidth = STROKE_WIDTH
             color = Color.RED
-            setShadowLayer(SHADOW_RADIUS, 0f, 0f, Color.BLACK)
         }
+    }
+
+    var round = 0f
+        private set
+
+    fun setRound(round: Float) {
+        this.round = round
+        invalidate()
+        invalidateOutline()
+    }
+
+    init {
+        initAttrs(attrs, context)
+        initPaints()
+        initOutlineProvider()
+    }
+
+    private fun initOutlineProvider() {
+        clipToOutline = true
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View?, outline: Outline?) {
+                view ?: return
+                outline ?: return
+                return outline.setRoundRect(0, 0, view.measuredWidth, view.measuredHeight, round)
+            }
+        }
+    }
+
+    private fun initPaints() {
+        shadowPaint.setShadowLayer(SHADOW_RADIUS, 0f, 0f, shadowColor)
+    }
+
+    private fun initAttrs(attrs: AttributeSet?, context: Context) {
+        attrs ?: return
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.InnerShadowView)
+        round = typedArray.getDimension(R.styleable.InnerShadowView_round, 0f)
+        shadowColor = typedArray.getColor(R.styleable.InnerShadowView_shadowColor, Color.BLACK)
+        typedArray.recycle()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        shadowLineRect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+        shadowLineRect =
+            RectF(RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())).also {
+                val inset = -(STROKE_WIDTH / 2f)
+                it.inset(inset, inset)
+            }
     }
 
-    private var shadowLineRect: RectF? = null
     override fun dispatchDraw(canvas: Canvas?) {
         super.dispatchDraw(canvas)
         canvas ?: return
@@ -38,14 +78,13 @@ class InnerShadowView @JvmOverloads constructor(
     }
 
     private fun drawInnerShadow(canvas: Canvas) {
-        shadowLineRect?.let { shadowLineRect ->
-            canvas.save()
-            val inset = -(STROKE_WIDTH / 2)
-            shadowLineRect.inset(inset, inset)
-            val clipRect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
-            canvas.clipRect(clipRect)
-            canvas.drawRect(shadowLineRect, shadowPaint)
-            canvas.restore()
-        }
+        canvas.save()
+        shadowLineRect?.let { canvas.drawRoundRect(it, round, round, shadowPaint) }
+        canvas.restore()
+    }
+
+    companion object {
+        const val SHADOW_RADIUS = 20f
+        const val STROKE_WIDTH = 20f
     }
 }
--- app/src/main/java/com/skit/cis/MainActivity.kt
+++ app/src/main/java/com/skit/cis/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.skit.cis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val shadowView = findViewById<InnerShadowView>(R.id.shadow_view)
+        val tvSize = findViewById<TextView>(R.id.tv_size)
+        val seekBar = findViewById<SeekBar>(R.id.seek_bar)
+        shadowView.post {
+            seekBar.max = min(shadowView.measuredHeight, shadowView.measuredWidth) / 2
+        }
+        seekBar.also {
+            it.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    tvSize.text = "round: $progress"
+                    shadowView.setRound(progress.toFloat())
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+        }
     }
 }
